3a2d864505daa87543c1ce27ea289c56a90c1589
Honore20 <avekorm@gmail.com>
2025-03-31 17:24:23 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = ''
-    for j in range(1, valeur + 1):
-        espaces = '&nbsp;' * (valeur - j)  # Espaces à gauche pour que la base soit droite
+    for j in range(valeur, 0, -1):  # Boucle de valeur à 1
+        espaces = '&nbsp;' * (valeur - j)  # Espaces pour aligner à droite
         etoiles += espaces + '*' * j + '<br>'
     return etoiles
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
