595433dd0687a7bd9347a2a81cfe45a280c38697
Honore20 <avekorm@gmail.com>
2025-03-31 17:26:10 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+from flask import json
 
+app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    etoiles = ''
-    for j in range(valeur, 0, -1):  # Boucle de valeur à 1
-        espaces = '&nbsp;' * (valeur - j)  # Espaces pour aligner à droite
-        etoiles += espaces + '*' * j + '<br>'
+    etoiles = '<pre>'
+    for j in range(1, valeur + 1):  
+        espaces = ' ' * (valeur - j)  
+        etoiles += espaces + '*' * (1 * j - 1) + '\n'
+    etoiles += '</pre>'
     return etoiles
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
